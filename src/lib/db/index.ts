10768bf439d28dc30b3365ba6de31239c73abf49
Punith1117 <punith11172006@gmail.com>
2026-06-16 10:14:12 +0530

    ci: add TEST_NODE_ENV_OVERRIDE=test environmental variable
    - this is done since Nextjs overrides NODE_ENV always irrespective of whether
    it is explicity present in .env.test

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,10 +4,16 @@ import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL!;
 
+// Explicitly check for an E2E flag, making intent very clear.
+// TEST_NODE_ENV_OVERRIDE exists because Nextjs hardcodes NODE_ENV to
+// production or development depending on command making it impossible to override "test" through NODE_ENV
+const isE2ETest = process.env.NODE_ENV === "test" || process.env.TEST_NODE_ENV_OVERRIDE === "test";
+
+// Determine SSL purely based on environment or connection string requirements
+const useSSL = process.env.NODE_ENV === "production" && !isE2ETest;
+
 const client = postgres(connectionString, {
-  ssl: process.env.NODE_ENV === "production"
-      ? "require"
-      : false,
+  ssl: useSSL ? "require" : false,
   max: 1,
 });
 
